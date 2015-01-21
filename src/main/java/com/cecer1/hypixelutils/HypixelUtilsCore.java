c555a19fe8cb00647c859db258bb6e15594f7d36
Cecer <Cecer1@cecer1.com>
2015-01-21 22:32:09 +0000

    Added actual class loading (I am far too lazy sometimes). Bumped version to 1.2.1 for bug fix release.

--- src/main/java/com/cecer1/hypixelutils/HypixelUtilsCore.java
+++ src/main/java/com/cecer1/hypixelutils/HypixelUtilsCore.java
@@ -47,7 +47,7 @@ import java.util.UUID;
 
 public class HypixelUtilsCore {
     public static final String MODID = "hypixelutils";
-    public static final String VERSION = "1.2.0";
+    public static final String VERSION = "1.2.1";
     public static final String CONFIG_SERVER = "http://www.cecer1.com/hypixelutils/cloudconfig";
     //public static final String CONFIG_SERVER = "http://hypixelutils.cecer1.com:8014";
     //public static final String CONFIG_SERVER = "http://localhost:8014";
--- src/main/java/com/cecer1/hypixelutils/LiteModHypixelUtils.java
+++ src/main/java/com/cecer1/hypixelutils/LiteModHypixelUtils.java
@@ -4,11 +4,13 @@ import com.cecer1.hypixelutils.features.cloudconfig.CloudConfigManager;
 import com.cecer1.hypixelutils.gui.GuiConfigManagerWrapper;
 import com.cecer1.modframework.common.Scheduler;
 import com.cecer1.modframework.common.events.EventManager;
+import com.cecer1.modframework.common.utils.MD5Checksum;
 import com.cecer1.modframework.common.utils.WorldDimension;
 import com.cecer1.modframework.liteloader.commands.LiteLoaderCommandRegister;
 import com.cecer1.modframework.liteloader.events.adapters.*;
 import com.mojang.realmsclient.dto.RealmsServer;
 import com.mumfrey.liteloader.*;
+import com.mumfrey.liteloader.core.LiteLoader;
 import com.mumfrey.liteloader.core.LiteLoaderEventBroker;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ServerData;
@@ -19,6 +21,14 @@ import net.minecraft.network.play.server.S07PacketRespawn;
 import net.minecraft.util.IChatComponent;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
@@ -66,6 +76,8 @@ public class LiteModHypixelUtils implements ChatFilter, Tickable, OutboundChatFi
 
     @Override
     public void init(File configFile) {
+        prepareLibs();
+        
         liteloaderCommandRegister = new LiteLoaderCommandRegister();
         HypixelUtilsCore.commandRegister = liteloaderCommandRegister;
 
@@ -95,6 +107,82 @@ public class LiteModHypixelUtils implements ChatFilter, Tickable, OutboundChatFi
         liteLoaderOnRenderAdapter = new LiteLoaderOnRenderAdapter(HypixelUtilsCore.eventManager);
         liteLoaderOnConnectAdapter = new LiteLoaderOnConnectAdapter(HypixelUtilsCore.eventManager);
     }
+
+
+    private void prepareLibs() {
+        Path libsDirPath = Paths.get(LiteLoader.getModsFolder().getAbsolutePath(), "libs");
+        File libsDirFile = libsDirPath.toFile();
+        if(!libsDirFile.exists()) {
+            libsDirFile.mkdirs();
+        } else {
+            if(!libsDirFile.isDirectory()) {
+                throw new RuntimeException(new IOException("Unable to create /mobs/libs directory. Conflicting file name! Refusing to clobber!"));
+            }
+        }
+        
+        File libFile;
+        
+        libFile = Paths.get(libsDirPath.toString(), "httpasyncclient-4.0.2.jar").toFile();
+        ensureLib(libFile, "3c0145d6ab072b8e755699c246b224a3", "http://www.cecer1.com/hypixelutils/libs/httpasyncclient-4.0.2.jar");
+
+        libFile = Paths.get(libsDirPath.toString(), "httpclient-4.3.6.jar").toFile();
+        ensureLib(libFile, "98ac3eaa1d681c3adc962d756ebed5a0", "http://www.cecer1.com/hypixelutils/libs/httpclient-4.3.6.jar");
+
+        libFile = Paths.get(libsDirPath.toString(), "httpcore-nio-4.3.2.jar").toFile();
+        ensureLib(libFile, "2523ba09110e521a02c54e271c06a93d", "http://www.cecer1.com/hypixelutils/libs/httpcore-nio-4.3.2.jar");
+
+        libFile = Paths.get(libsDirPath.toString(), "httpmime-4.3.6.jar").toFile();
+        ensureLib(libFile, "ec661ad97a37cca0a91186625cf1fcba", "http://www.cecer1.com/hypixelutils/libs/httpmime-4.3.6.jar");
+
+        libFile = Paths.get(libsDirPath.toString(), "json-20090211.jar").toFile();
+        ensureLib(libFile, "333139fffc6c9d4bc3d2495d9613f092", "http://www.cecer1.com/hypixelutils/libs/json-20090211.jar");
+
+        libFile = Paths.get(libsDirPath.toString(), "unirest-java-1.3.27.jar").toFile();
+        ensureLib(libFile, "ec4cdf09634f33c15d7c2972a83cd286", "http://www.cecer1.com/hypixelutils/libs/unirest-java-1.3.27.jar");
+    }
+    
+    private void ensureLib(File file, String expectedChecksum, String downloadUrl) {
+        boolean fileExisted = file.exists();
+        if(!fileExisted) {
+            try {
+                System.out.println(file.getAbsolutePath() + " does not exist. Downloading from " + downloadUrl);
+                URL website = new URL(downloadUrl);
+                ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+                FileOutputStream fos = new FileOutputStream(file);
+                fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+                System.out.println("Downloaded finished. Comparing checksum.");
+            } catch (MalformedURLException e) {
+                throw new RuntimeException(e);
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        try {
+            if(MD5Checksum.compareMD5Checksum(file.getAbsolutePath(), expectedChecksum)) {
+                System.out.println("Successful checksum for " + file.getAbsolutePath() + " - Expected and calculated " + expectedChecksum);
+            } else {
+                System.out.println("Failed checksum for " + file.getAbsolutePath() + " - Expected "+ expectedChecksum + " but calculated " + expectedChecksum);
+                if(fileExisted) {
+                    throw new RuntimeException(new IOException("Checksum mismatch on " + file.getAbsolutePath() + " - Corrupt? - Refusing to clobber! Please manually resolve this issue."));
+                } else {
+                    file.delete();
+                    throw new RuntimeException(new IOException("Checksum mismatch on downloaded " + file.getAbsolutePath() + " - Corrupt? - File deleted. Restart the game to retry."));
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Failed to calculate checksum for " + file.getAbsolutePath() + " - WARNING: The file may be corrupt but HypixelUtils will assume it is fine!");
+        }
+
+        try {
+            LiteLoader.getClassLoader().addURL(file.toURI().toURL());
+            System.out.println("Added " + file.toURI().toURL() + " to LiteLoader ClassLoader.");
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
 
     @Override
     public void upgradeSettings(String version, File configPath, File oldConfigPath) {
